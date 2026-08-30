a039b0820cfcfc18e6109bbdb6d895d1e3b30382
Karim8112 <kareem.happal@gmail.com>
2026-08-30 15:35:39 +0300

    changing server

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import app from "./app.js";
+import dotenv from "dotenv";
 import { config } from "./constants/core.js";
+
+dotenv.config({ path: "./config.env" });
+
+//
 
 app.listen(config.port, config.host, () => {
   console.log(`App running on port ${config.port}...`);
